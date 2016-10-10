5a6b0dccd90f07d1bfc5b5e71236608010c0d0bf
raboy1997 <maks.rublya@mail.ru>
2016-10-10 20:23:28 +0300

    add database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009184158) do
+ActiveRecord::Schema.define(version: 20161010161800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20161009184158) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "post_link"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
